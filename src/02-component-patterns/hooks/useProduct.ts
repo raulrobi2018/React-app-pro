c0ec44c228338e83fca5f2a43c978a222948b196
Raul Rodriguez <raulrobi@gmail.com>
2022-12-17 21:06:36 -0300

    Control props alternative implemented

--- src/02-component-patterns/hooks/useProduct.ts
+++ src/02-component-patterns/hooks/useProduct.ts
@@ -7,23 +7,12 @@ interface useProductProps {
     value?: number;
 }
 
-
 export const useProduct = ({ onChange, product, value = 0 }: useProductProps) => {
 
     const [counter, setCounter] = useState(value);
 
-    const isControlled = useRef(!!onChange)
-
-    useEffect(() => {
-        setCounter(value);
-    }, [value])
-
-
     const increaseBy = (value: number) => {
-        if (isControlled.current) {
-            //Este símbolo "!", le dice a typescript que el posible undefined está controlado
-            return onChange!({ count: value, product })
-        }
+
         //Aquí se aumenta el counter de esta manera para asegurarnos que el valor seteado
         //nunca sea menor a 0
         const newCounter = Math.max(counter + value, 0)
--- src/02-component-patterns/hooks/useShoppingCart.tsx
+++ src/02-component-patterns/hooks/useShoppingCart.tsx
@@ -14,38 +14,22 @@ export const useShoppingCart = () => {
         product: Product;
     }) => {
         setShoppingCart((prevShoppingCart) => {
-            const productInCart: ProductInCart = prevShoppingCart[
-                product.id
-            ] || {...product, count: 0};
+            //Cuando llega a 0 hay que quitarle el producto para que no lo muestre en pantalla
+            if (count === 0) {
+                delete prevShoppingCart[product.id];
+                //Desestructuro el producto que tenga la key igual a la que estoy llegando a 0
+                //y además saco todo lo demás que venga en el array de objetos con el ...rest
+                const {[product.id]: toDelete, ...rest} = prevShoppingCart;
 
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count;
-                return {
-                    ...prevShoppingCart,
-                    [product.id]: productInCart
-                };
+                //Luego retorno el rest sin el producto que llegó a 0
+                return rest;
             }
 
-            //Delete the product
-            const {[product.id]: toDelete, ...rest} = prevShoppingCart;
-            return rest;
-
-            //Cuando llega a 0 hay que quitarle el producto para que no lo muestre en pantalla
-            // if (count === 0) {
-            //     delete prevShoppingCart[product.id];
-            //     //Desestructuro el producto que tenga la key igual a la que estoy llegando a 0
-            //     //y además saco todo lo demás que venga en el array de objetos con el ...rest
-            //     const {[product.id]: toDelete, ...rest} = prevShoppingCart;
-
-            //     //Luego retorno el rest sin el producto que llegó a 0
-            //     return rest;
-            // }
-
-            // return {
-            //     ...prevShoppingCart,
-            //     //Aquí le pasamos la key computada
-            //     [product.id]: {...product, count}
-            // };
+            return {
+                ...prevShoppingCart,
+                //Aquí le pasamos la key computada
+                [product.id]: {...product, count}
+            };
         });
     };
     return {
